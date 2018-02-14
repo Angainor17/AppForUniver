3eeccd79277b6879bb282ad85799e4280000cb48
Voronin Igor <angainor17@gmail.com>
2018-02-14 11:24:26 +0300

    vibro and browser

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -1,21 +1,14 @@
 package com.example.admin.myapplication;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonVibro;
+    Button buttonVibration;
     Button buttonBrowser;
     Button buttonVibro2;
     Button buttonVibro3;
@@ -27,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonVibro = findViewById(R.id.buttonVibro);
-        buttonVibro.setOnClickListener(new View.OnClickListener() {
+        buttonVibration = findViewById(R.id.buttonVibro);
+        buttonVibration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), VibroActivity.class);
